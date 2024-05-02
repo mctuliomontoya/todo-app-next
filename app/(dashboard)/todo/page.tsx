d552b6e76554437bb02ea6b14fda5a30bd27673d
mctuliomontoya <mctulio.2@gmail.com>
2024-05-02 02:49:00 -0700

    style: add background

--- app/(dashboard)/todo/page.tsx
+++ app/(dashboard)/todo/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Container from "@/app/components/Container";
 import TodoList from "./components/TodoList";
 import React from "react";
 
@@ -10,7 +9,7 @@ export default function Products() {
   const handleDirectDelete = () => setDirectDelete(!directDelete);
 
   return (
-    <main className="min-h-screen bg-black flex justify-center items-center ">
+    <main className="background-test min-h-screen flex justify-center items-center ">
       <div className="box flex items-center border-[2px] border-transparent rounded-2xl justify-center min-w-fit w-1/3 h-1/2 max-w-screen-lg p-6">
         <TodoList></TodoList>
       </div>
